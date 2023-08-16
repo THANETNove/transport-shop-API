c17dd4ae89b4da34546b635efe9bc0f1a99eb76d
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-08-17 00:44:52 +0700

    test api getUser

---
+++ getUser.php
@@ -0,0 +1,36 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "call_credit_checker";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_GET)) {
+    if ($_GET['isAdd'] == 'true') {
+        $sql = "SELECT *  FROM users";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $data = [];
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+            echo json_encode($data);
+        } else {
+            echo "0 results";
+        }
+    } else echo "Welcome Master UNG";
+}
+$conn->close();
