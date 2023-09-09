0be15b1d01d13f5f8536f47625dc86f9c44fa144
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-09-09 22:23:12 +0700

    a

--- db_config.php
+++ db_config.php
@@ -3,3 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "transport_shop";
+/* $servername = "localhost";
+$username = "medocarg_medocarg";
+$password = "12345678";
+$dbname = "medocarg_medocarg"; */
